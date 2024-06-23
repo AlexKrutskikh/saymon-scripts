c906aa8fcf9b6718f88c6133aa26ef22aea065ec
Alex Krutskikh <krutskih.ay@gmail.com>
2024-06-23 03:18:34 +0300

    Delete print

--- Unit_Bulk_Operations/test_bulk.py
+++ Unit_Bulk_Operations/test_bulk.py
@@ -10,5 +10,4 @@ for строка in df_result_sql.iterrows():
     print(f"rsm_obj_to_create_under: {строка[1]['rsm_obj_to_create_under']}")
     print(f"service_group: {строка[1]['service_group']}")
     print(f"name: {строка[1]['name']}")
-    print(f"id: {строка[1]['id']}")
 
