401b84e90f2b3469b4a0d752b512c487123c6f2f
Alex Krutskikh <krutskih.ay@gmail.com>
2024-09-02 06:23:51 +0300

    tesy change

--- Unit_Bulk_Operations/test_bulk.py
+++ Unit_Bulk_Operations/test_bulk.py
@@ -1,11 +1,11 @@
 import pandas as pd
-import os
+
 
 # Прочитаем CSV файл в DataFrame
 file_path = './reports/df_result_sql.csv'
 df_result_sql = pd.read_csv(file_path)
 
-# Итерация по строкам DataFrame
+
 for строка in df_result_sql.iterrows():
     print(f"rsm_obj_to_create_under: {строка[1]['rsm_obj_to_create_under']}")
     print(f"service_group: {строка[1]['service_group']}")
